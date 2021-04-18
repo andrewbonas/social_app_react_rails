5876c16f26ac0ca5a77dd692321746dc51bcf526
Andrew Bonas <aabonas@hotmail.com>
2021-04-18 09:41:22 -0700

    spinner modified

--- app/javascript/packs/App.jsx
+++ app/javascript/packs/App.jsx
@@ -1,13 +1,10 @@
 import React, { useState, useEffect } from "react";
 import Posts from "./components/Posts";
 import PostForm from "./components/PostForm";
-import Spinner from "./components/Spinner";
-
 
 
 const App = () => {
   const [posts, setPosts] = useState([]);
-  const [loading, setLoading] = useState(false);
 
 
   const updatePost = (post) => {
@@ -21,34 +18,22 @@ const App = () => {
   }
   }
   useEffect(() => {
-   setLoading(true)
-
     fetch(`/api/v1/posts`)
       .then((response) => {
         return response.json();
       })
       .then((data) => setPosts(data))
-      .catch((error) => console.log(error.message))
-      setLoading(false)
+      .catch((error) => console.log(error.message));
   }, []);
 
   return (
     <div>
-      {!loading && ( 
-        <div>
       <PostForm updatePost={updatePost}/>
       <div>
         {posts.map((post) => (
           <Posts key={post.id} post={post} updatePost={updatePost} />
         ))}
       </div>
-      </div>
-      )}
-      {loading && (
-        <div>
-          <Spinner/>
-          </div>
-      )}
     </div>
   );
 };
