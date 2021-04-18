7b44304aa8b9b55a7470dd6586ccc5028e4477ce
Andrew Bonas <aabonas@hotmail.com>
2021-04-18 10:02:06 -0700

    followers added

--- app/javascript/packs/App.jsx
+++ app/javascript/packs/App.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
 import Posts from "./components/Posts";
 import PostForm from "./components/PostForm";
+import Spinner from "./components/Spinner";
 
 
 const App = () => {
   const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
 
 
   const updatePost = (post) => {
@@ -18,22 +20,33 @@ const App = () => {
   }
   }
   useEffect(() => {
+    setLoading(true)
     fetch(`/api/v1/posts`)
       .then((response) => {
         return response.json();
       })
       .then((data) => setPosts(data))
-      .catch((error) => console.log(error.message));
+      .catch((error) => console.log(error.message))
+      setLoading(false)
   }, []);
 
   return (
     <div>
+       {!loading && ( 
+        <div>
       <PostForm updatePost={updatePost}/>
       <div>
         {posts.map((post) => (
           <Posts key={post.id} post={post} updatePost={updatePost} />
         ))}
       </div>
+      </div>
+      )}
+      {loading && (
+        <div>
+          <Spinner/>
+          </div>
+      )}
     </div>
   );
 };
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  acts_as_followable
+  acts_as_follower
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
   has_many :posts, dependent: :destroy
+  
 end
